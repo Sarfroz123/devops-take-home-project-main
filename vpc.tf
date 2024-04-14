148c6fbc0012b7ee5695a1c57579469e662fe8f9
Sarfroz Basha <sbasha@tcd1.com>
2024-04-14 08:25:48 -0700

    Modified the files

--- vpc.tf
+++ vpc.tf
@@ -17,25 +17,19 @@ resource "aws_vpc" "main" {
 # Create your subnets here
 # =========================
 
-module "public_subnet" {
-  source  = "terraform-aws-modules/subnet/aws"
-  version = "3.0.0"
-
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 4, 0)
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, 0)
   map_public_ip_on_launch = true
-  availability_zone       = var.aws_region
+  availability_zone = var.aws_region
 
-  tags = module.base_label.tags
+  # Other subnet configuration options...
 }
 
-module "private_subnet" {
-  source  = "terraform-aws-modules/subnet/aws"
-  version = "3.0.0"
-
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 4, 1)
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, 1)
   map_public_ip_on_launch = false
-  availability_zone       = var.aws_region
-  tags = module.base_label.tags
+  availability_zone = var.aws_region
+
 }
